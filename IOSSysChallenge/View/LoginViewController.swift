efe97cfce79f424006f49f9e37d9662c92c4cdf5
Ruyther Costa <ruyther.costa@foton.la>
2020-10-19 20:56:50 -0300

    Configuring git

---
+++ IOSSysChallenge/View/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  IOSSysChallenge
+//
+//  Created by Ruyther Costa on 19/10/20.
+//
+
+import Foundation
---
+++ IOSSysChallenge/ViewModel/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  IOSSysChallenge
+//
+//  Created by Ruyther Costa on 19/10/20.
+//
+
+import Foundation
